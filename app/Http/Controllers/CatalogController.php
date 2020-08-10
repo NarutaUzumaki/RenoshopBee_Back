189bf70350fa6e13f62b3567f2fbc2db0eadfc04
NarutaUzumaki <orion.nikolaev@gmail.com>
2020-08-10 17:28:19 +0300

    api for frontend

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Product;
-use http\Env\Response;
+use App\catalog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class ProductController extends Controller
+class CatalogController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -16,27 +15,24 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = DB::table('products')->get();
+        $catalog = DB::table('catalogs')->get();
 
         return response()->json([
-            'products'=>$products
+            'catalog'=>$catalog
         ]);
     }
 
-    public function getProductById($id){
-        $products = DB::table('products')->find($id);
 
-        return response()->json([
-            'products'=>$products
-        ]);
-    }
 
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-
+    public function create()
+    {
+        //
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -52,10 +48,10 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Product  $product
+     * @param  \App\catalog  $catalog
      * @return \Illuminate\Http\Response
      */
-    public function show(Product $product)
+    public function show(catalog $catalog)
     {
         //
     }
@@ -63,19 +59,22 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Product  $product
+     * @param  \App\catalog  $catalog
      * @return \Illuminate\Http\Response
      */
-
+    public function edit(catalog $catalog)
+    {
+        //
+    }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Product  $product
+     * @param  \App\catalog  $catalog
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request, catalog $catalog)
     {
         //
     }
@@ -83,17 +82,11 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Product  $product
+     * @param  \App\catalog  $catalog
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product)
+    public function destroy(catalog $catalog)
     {
         //
-    }
-
-    public function getProducts(){
-        $products = DB::table('products')->get();
-
-        return view('test.index', ['products'=>$products]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::resource('product', 'ProductController', ['except' => ['edit', 'show', 'store']]);
-
+Route::resource('catalog', 'CatalogController', ['except' => ['edit', 'show', 'store']]);
+Route::get('product/{id}', 'ProductController@getProductById');
